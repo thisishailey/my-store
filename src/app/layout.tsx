c6dc044dd1af2b61ab4ff85b529e79bc9fff42f8
Hailey Kim <kfbkhw@gmail.com>
2024-03-23 08:52:22 +0900

    fix: re-commit theme updates

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import { Providers } from './providers';
 import Header from '@/components/common/layout/Header';
 import Footer from '@/components/common/layout/Footer';
 import './globals.css';
@@ -19,17 +20,17 @@ export default function RootLayout({
 }: Readonly<{
     children: React.ReactNode;
 }>) {
+    const htmlClasses = `p-0 overflow-auto bg-neutral-50 dark:bg-neutral-900`;
     const bodyClasses = `${inter.className} bg-neutral-50 text-neutral-950 dark:bg-neutral-900 dark:text-neutral-50`;
 
     return (
-        <html
-            lang="en"
-            className="p-0 overflow-auto bg-neutral-50 dark:bg-neutral-900"
-        >
+        <html lang="en" className={htmlClasses} suppressHydrationWarning>
             <body className={bodyClasses}>
-                <Header />
-                {children}
-                <Footer />
+                <Providers>
+                    <Header />
+                    {children}
+                    <Footer />
+                </Providers>
             </body>
         </html>
     );
